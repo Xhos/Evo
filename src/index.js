409ac86ad956a670cce72f3b1fb82309751e5a67
Mark Kudriavtsev <xxhos@pm.me>
2023-12-21 22:22:32 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+const eventHandler = require('./handlers/eventHandler');
+
+const client = new Client({ 
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.MessageContent,
+  ]
+});
+
+eventHandler(client);
+
+client.login(process.env.DISCORD_TOKEN);
