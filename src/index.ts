9e2e3d622de6f470a2bc6514721a32324fe04363
Mark Kudriavtsev <xxhos@pm.me>
2024-03-02 20:37:54 -0500

    updated log functiom

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,9 @@
-// External module imports
-import * as oracledb from 'oracledb';
 import { Client, IntentsBitField } from 'discord.js';
 import dotenv from 'dotenv';
-
-// Internal module imports
-import { log, logLevel } from './utils/log';
 import eventHandler from './handlers/eventHandler';
 
-// Load environment variables
 dotenv.config();
 
-// Database configuration
-const dbConfig: oracledb.ConnectionAttributes = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  connectString: process.env.DB_CONNECTION_STRING,
-};
-
-// Database connection class
-class DBConnection {
-  public async initializeDBConnection(): Promise<oracledb.Connection> {
-    try {
-      const connection = await oracledb.getConnection(dbConfig);
-      log('Connected to the database', logLevel.Info);
-      return connection;
-    } catch (err) {
-      log('Error occurred while connecting to the database', logLevel.Error);
-      throw err;
-    }
-  }
-}
-
-// Initialize database connection
-const dbConnection = new DBConnection();
-dbConnection.initializeDBConnection();
-
-// Initialize Discord client
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
@@ -46,8 +14,6 @@ const client = new Client({
   ],
 });
 
-// Handle events
 eventHandler(client);
 
-// Log in to Discord
 client.login(process.env.DISCORD_TOKEN);
--- src/utils/log.ts
+++ src/utils/log.ts
@@ -16,6 +16,36 @@ export enum logLevel {
 }
 
 export async function log(message: string, level: logLevel = logLevel.Info) {
+  let colorizedMessage;
+  switch (level) {
+    case logLevel.Error:
+      colorizedMessage = `\x1b[31m[ERROR] ${message}\x1b[0m`; // Red
+      break;
+    case logLevel.Warn:
+      colorizedMessage = `\x1b[33m[WARN] ${message}\x1b[0m`; // Yellow
+      break;
+    case logLevel.Info:
+      colorizedMessage = `\x1b[36m[INFO] ${message}\x1b[0m`; // Cyan
+      break;
+    case logLevel.Debug:
+      colorizedMessage = `\x1b[35m[DEBUG] ${message}\x1b[0m`; // Magenta
+      break;
+    default:
+      colorizedMessage = `[${level}] ${message}`;
+  }
+
+  console.log(colorizedMessage);
+
+  if (level === logLevel.Error || level === logLevel.Warn) {
+    try {
+      await webhookClient.send(`[${level}] ${message}`);
+    } catch (error: any) {
+      console.error(
+        `Failed to send message to Discord webhook: ${error.message}`
+      );
+    }
+  }
+
   const currentDate = new Date();
   const formattedDate = currentDate
     .toLocaleString('en-GB', {
@@ -29,40 +59,13 @@ export async function log(message: string, level: logLevel = logLevel.Info) {
     .replace(/,/g, '')
     .replace(/\//g, '.');
 
-  const formattedMessage = `[${level}] (${formattedDate}) ${message}\n`;
-
-  let colorizedMessage;
-  switch (level) {
-    case logLevel.Error:
-      colorizedMessage = `\x1b[31m${formattedMessage}\x1b[0m`; // Red
-      break;
-    case logLevel.Warn:
-      colorizedMessage = `\x1b[33m${formattedMessage}\x1b[0m`; // Yellow
-      break;
-    case logLevel.Info:
-      colorizedMessage = `\x1b[36m${formattedMessage}\x1b[0m`; // Cyan
-      break;
-    default:
-      colorizedMessage = formattedMessage;
-  }
-
-  console.log(colorizedMessage.replace(/\n$/, ''));
-
-  if (level === logLevel.Error || level === logLevel.Warn) {
-    try {
-      await webhookClient.send(formattedMessage);
-    } catch (error: any) {
-      console.error(
-        `Failed to send message to Discord webhook: ${error.message}`
-      );
-    }
-  }
-
   const logFileName = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}.log`;
   const logFilePath = path.join(__dirname, '..', 'logs', logFileName);
 
+  const logFileMessage = `[${level}] (${formattedDate}) ${message}`;
+
   try {
-    await fs.appendFile(logFilePath, formattedMessage + '\n');
+    await fs.appendFile(logFilePath, logFileMessage + '\n');
   } catch (error: any) {
     console.error(`Failed to write to log file: ${error.message}`);
   }
