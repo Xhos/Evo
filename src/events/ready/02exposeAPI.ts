4f972fac90da4e17629eb3d388a17f758439eee0
Mark Kudriavtsev <mkudriavtsev@myseneca.ca>
2024-03-26 23:29:20 -0400

    more api worl

--- src/events/ready/02exposeAPI.ts
+++ src/events/ready/02exposeAPI.ts
@@ -2,10 +2,10 @@ import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
 import { ServerUnaryCall, sendUnaryData } from '@grpc/grpc-js';
 import { Player } from '../../utils/player';
+import { Queue } from '../../utils/queue';
 import { logLevel, log } from '../../utils/log';
 
 module.exports = async (client: any) => {
-  // Load the .proto file
   const packageDefinition = protoLoader.loadSync('evoBot.proto', {
     keepCase: true,
     longs: String,
@@ -16,33 +16,47 @@ module.exports = async (client: any) => {
 
   const evoBotProto = grpc.loadPackageDefinition(packageDefinition).evoBot as any;
 
-  // Implement the join function
   async function join(call: ServerUnaryCall<any, any>, callback: sendUnaryData<any>) {
-    const guildID = call.request.user_id;
+    const guildID = call.request.guild_id;
     const channelID = call.request.channel_id;
-    // log the request
-    log(
-      `Got a request to join the voice channel in guild ${guildID} and channel ${channelID}`,
-      logLevel.Info
-    );
-    // Get the queue and player for the guild
-    let player = Player.getPlayer(guildID);
 
-    // Join the voice channel
+    log(`GRPC api call to join guild ${guildID}, channel ${channelID}`, logLevel.Info);
+
+    let player = await Player.getPlayer(guildID);
     await player.join(channelID);
 
     callback(null, { result: 'Joined the voice channel' });
   }
 
-  // Create a gRPC server
+  async function play(call: ServerUnaryCall<any, any>, callback: sendUnaryData<any>) {
+    const guildID = call.request.guild_id;
+
+    log(`GRPC api call to play in track guild ${guildID}`);
+
+    let player = await Player.getPlayer(guildID);
+    await player.play();
+
+    callback(null, { result: 'Started playing' });
+  }
+
+  async function add(call: ServerUnaryCall<any, any>, callback: sendUnaryData<any>) {
+    const guildID = call.request.guild_id;
+    const link = call.request.link;
+
+    const queue = Queue.getQueue(guildID);
+    try {
+      await queue.add(link, 'username'); // Replace 'username' with the actual username
+      callback(null, { result: 'Track added to the queue!' });
+    } catch (error: any) {
+      log(error, logLevel.Error);
+      callback(null, { result: 'There was an error adding the track to the queue.' });
+    }
+  }
   const server = new grpc.Server();
-
-  // Add the EvoBot service to the server
-  server.addService(evoBotProto.EvoBot.service, { join: join });
-
-  // Start the server
+  server.addService(evoBotProto.EvoBot.service, { join: join, add: add, play: play });
   server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
-    console.log('Server listening on 0.0.0.0:50051');
+    log('API server listening on 0.0.0.0:50051');
   });
 };
+
 export {};
